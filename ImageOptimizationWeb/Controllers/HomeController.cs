75e2141e7d7e5b8caabf771539bcab29ec38ac69
Dean <contact@deanhume.com>
2014-05-29 15:13:32 +0100

    - Added SEO details & new logo

--- ImageOptimizationWeb/Controllers/HomeController.cs
+++ ImageOptimizationWeb/Controllers/HomeController.cs
@@ -6,36 +6,50 @@ namespace ImageOptimizationWeb.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.PageTitle = "Home";
+
             return View();
         }
 
         public ActionResult Compressive()
         {
+            ViewBag.PageTitle = "Compressive";
+
             return View();
         }
         
         public ActionResult DataUri()
         {
+            ViewBag.PageTitle = "Data Uris";
+
             return View();
         }
 
         public ActionResult WebP()
         {
+            ViewBag.PageTitle = "WebP";
+
             return View();
         }
 
         public ActionResult LazyLoad()
         {
+            ViewBag.PageTitle = "LazyLoad";
+
             return View();
         }
 
         public ActionResult Sprites()
         {
+            ViewBag.PageTitle = "Sprites";
+
             return View();
         }
 
         public ActionResult Tools()
         {
+            ViewBag.PageTitle = "Optimisation Tools";
+
             return View();
         }
     }
